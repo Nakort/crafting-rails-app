3b31d414f3e1f8d204c38965b02488386b5d15e3
Nakort <nakort@gmail.com>
2012-04-26 14:12:00 -0430

    Added mail form plugin (Chapter 4)

---
+++ mail_form/test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '1b8d9181c08289484c6e936dc152c9bb61335f0d9bcfa539507fac1ff85ff0a9e920515aa8417eeb9ce1541866a20a80d940b8ce345bca67edd31117aebe157a'
